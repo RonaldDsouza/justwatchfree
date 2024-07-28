9b06d36badd99c9d92c310b0200dd843faa3f11d
mxplayermovies <46235644+RonaldDsouza@users.noreply.github.com>
2024-07-28 11:51:50 +0530

    YES GO

--- pages/_app.js
+++ pages/_app.js
@@ -52,21 +52,6 @@ import Script from 'next/script';
 import { useEffect } from 'react';
 
 function Application({ Component, pageProps }) {
-  // useEffect(() => {
-  //   const setupWebpushr = () => {
-  //     if (typeof window.webpushr !== 'undefined') {
-  //       window.webpushr('setup', {
-  //         key: 'BDeLBmbVL39XWa_fEU4TTZ5OFjYr0zLf_PZN6CLLEtCdxOsDYdH6TIWC1ltmT8A4QdXsd8zVbN3izqMFubKPW_k',
-  //       });
-  //     }
-  //   };
-
-  //   const script = document.createElement('script');
-  //   script.src = 'https://cdn.webpushr.com/app.min.js';
-  //   script.async = true;
-  //   script.onload = setupWebpushr;
-  //   document.body.appendChild(script);
-  // }, []);
 
   return (
     <div className='center'>

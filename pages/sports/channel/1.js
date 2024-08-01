823c8f1c3ef551c436b62c4e8383621e95a1eb23
mxplayermovies <46235644+RonaldDsouza@users.noreply.github.com>
2024-08-02 04:55:22 +0530

    re submit

--- pages/sports/channel/1.js
+++ pages/sports/channel/1.js
@@ -20,7 +20,7 @@ const Ch01 = ({ sports }) => {
     <div>
       <Head>
         <title>Watch Sky Sport News HD DE | JWF™</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <meta name='robots' content='index, follow' />
         <meta name='revisit-after' content='1 days' />

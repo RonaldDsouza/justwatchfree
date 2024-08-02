3c44463de9ad6a504164d8f73818b6cf04e77a88
mxplayermovies <46235644+RonaldDsouza@users.noreply.github.com>
2024-08-03 05:05:07 +0530

    YES NOW

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://justwatchfree.vercel.app/',
+    changefreq: 'daily',
+    priority: 2.00,
+    // generateRobotsTxt: true,
+    sitemapSize: 7000,
+  }
